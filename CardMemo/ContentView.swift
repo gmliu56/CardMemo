3dc95e13bef66a94a5dcb3d00c144cc3a183c2b5
gmliu56 <bz86nick@hotmail.com>
2024-02-21 00:22:38 -0800

    experimental stack

--- CardMemo/ContentView.swift
+++ CardMemo/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! Testing")
+        VStack(alignment: .leading){
+            HStack{
+                Image("amex_cobalt")
+                    .cornerRadius(10)
+                    .aspectRatio(1.5, contentMode: .fit)
+                Spacer()
+                VStack(alignment: .leading){
+                    Text("5X Grocery")
+                    Text("5X Restaurant")
+                }
+                
+            }
+            Text("Amex Cobalt")
+                .fontWeight(.bold)
+        }.padding()
+        
+        VStack{
+            Text("Other Cards")
         }
-        .padding()
+        
+        
     }
 }
 
