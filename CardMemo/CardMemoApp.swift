257f5ed5e93caaebfa1e4e4d9174711240d64779
N1kken86 <N1kken86@cs.sfu.ca>
2024-02-20 22:39:57 -0800

    Initial Commit

---
+++ CardMemo/CardMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardMemoApp.swift
+//  CardMemo
+//
+//  Created by Nick Liu on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct CardMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
